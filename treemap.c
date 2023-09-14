199006bccd3d18588130400cd3a58eca5a8c1cc1
Replit user <>
2023-09-14 18:28:40 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -48,14 +48,11 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2))
   mapa -> lower_than = lower_than;
 
   return mapa;
-  //new->lower_than = lower_than;
+  new->lower_than = lower_than;
   return NULL;
 }
 void insertTreeMap(TreeMap * tree, void* key, void * value)
 {
-  if (searchTreeMap(tree, key) != NULL) {
-        return; // Clave duplicada, no se permite en el TreeMap.
-    }
   TreeNode * newNode = createTreeNode(key, value);
   if(tree -> root == NULL)
   {
